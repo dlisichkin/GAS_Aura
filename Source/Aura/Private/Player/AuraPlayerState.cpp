e1060b22374c4ecfa9db8fdcc9300f25600c46de
d.lisichkin <lisichkin.d@gmail.com>
2025-05-09 20:08:01 +0300

    Constructing the AbilitySystemComponent and AttributeSet

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,9 +3,22 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "Aura/Aura.h"
 
 AAuraPlayerState::AAuraPlayerState()
 {
 	SetNetUpdateFrequency(PLAYER_STATE_NET_UPDATE_FREQUENCY);
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
