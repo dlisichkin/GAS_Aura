e6c9cd22e7b5f74165de19e21551c346b9735d3d
d.lisichkin <lisichkin.d@gmail.com>
2025-07-27 19:06:16 +0300

    153. TStructOpsTypeTraits

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -20,6 +20,19 @@ public:
 		return FGameplayEffectContext::StaticStruct();
 	}
 
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
 	
@@ -29,4 +42,14 @@ protected:
 
 	UPROPERTY()
 	bool bIsBlockedHit = false;
+};
+
+template<>
+struct  TStructOpsTypeTraits<FAuraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
