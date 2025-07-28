22a8d89e6259a14defc007c96842ed35f816e1c6
d.lisichkin <lisichkin.d@gmail.com>
2025-07-28 19:33:06 +0300

    156. Floating Text Color

--- Source/Aura/Public/UI/Widget/DamageTextComponent.h
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -15,5 +15,5 @@ class AURA_API UDamageTextComponent : public UWidgetComponent
 	GENERATED_BODY()
 public:
 	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
-	void SetDamageText(float Damage);
+	void SetDamageText(float Damage, bool bBlockedHit, bool bCriticalHit);
 };
